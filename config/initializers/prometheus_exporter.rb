ac859c96265049ecc4828f05f951129b019895c8
Neeraj Shukla <shklnrj@gmail.com>
2020-07-21 09:59:39 +0530

    Run it by docker compose.

--- config/initializers/prometheus_exporter.rb
+++ config/initializers/prometheus_exporter.rb
@@ -1,6 +1,8 @@
 unless Rails.env == "test"
   require 'prometheus_exporter/middleware'
-
+  # require 'prometheus_exporter/client'
+  # client = PrometheusExporter::Client.new(host: ENV.fetch('PROMETHEUS_EXPORTER_HOST'))
+  # PrometheusExporter::Client.default = client
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift PrometheusExporter::Middleware
 end
